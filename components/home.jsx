a74e0daca2292b771a81aa36ad7a5449f84cdff3
brytran <Btran21003@gmail.com>
2024-01-28 00:33:49 -0500

    Updated home screen

--- components/home.jsx
+++ components/home.jsx
@@ -2,7 +2,51 @@ import "./home.css";
 function Home() {
   return (
     <span>
-      <p className="titleText spotify-text"> Spotify MyPage</p>
+      <p className="titleText spotify-text">
+        <b>Spotify MyList</b>
+      </p>
+      <p className="spotify-text descriptionText">
+        Discover music you've never seen!
+      </p>
+      <table>
+        <tr>
+          <td className="stepItem">
+            <img src="../src/assets/logo2.png"></img>
+            <p className="spotify-text stepText-modified">
+              <b>1.</b>
+            </p>
+            <p className="spotify-text stepText"> Log in to Spotify</p>
+          </td>
+          <td className="stepItem">
+            <img src="../src/assets/logo3.png"></img>
+            <p className="spotify-text stepText-modified">
+              <b>2.</b>
+            </p>
+            <p className="spotify-text stepText">
+              {" "}
+              Choose up to five music genres
+            </p>
+          </td>
+          <td className="stepItem">
+            <img src="../src/assets/logo4.png"></img>
+            <p className="spotify-text stepText-modified">
+              <b>3.</b>
+            </p>
+            <p className="spotify-text stepText"> Get your curated playlist</p>
+          </td>
+          <td className="stepItem">
+            <img src="../src/assets/logo5.png"></img>
+
+            <p className="spotify-text stepText-modified">
+              <b>4.</b>
+            </p>
+            <p className="spotify-text stepText">
+              {" "}
+              Choose your playlist cover and title
+            </p>
+          </td>
+        </tr>
+      </table>
     </span>
   );
 }
